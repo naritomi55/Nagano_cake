6441b485c485e331ef5be64ccfbf0de3efd01fbb
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-02 05:55:52 +0000

    [Add]ジャンル編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :genres, only: [:index, :edit, :create, :update]
+  end
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

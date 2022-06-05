27ce92a3e9aea72298a12ec56bfb7f67e8096a2c
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-05 11:25:30 +0000

    [Add]商品編集機能を実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,9 +1,12 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.page(params[:page])
   end
 
   def new
     @item = Item.new
+    @genres = Genre.all
+    @genres.first
   end
 
   def create
@@ -16,13 +19,23 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+    @genres.first
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
 
   private
     def item_params
-      params.require(:item).permit(:name, :introduction, :genre_id, :price, :is_active)
+      params.require(:item).permit(:name, :introduction, :genre_id, :price, :image, :is_active)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,16 @@
 class Item < ApplicationRecord
+
   has_one_attached :image
   belongs_to :genre
+
+  def add_tax_price
+    (self.price * 1.10).round
+  end
 
   validates :name, presence: true
   validates :introduction, presence: true
   validates :genre_id, presence: true
   validates :price, presence: true
-  validates :is_active, presence: true
+  validates :is_active, inclusion: [true, false]
+
 end

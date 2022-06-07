7a6db44651a47b1ae28e8f11f61454972ae71005
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-07 04:42:36 +0000

    [Add]会員情報編集画面実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,13 +4,24 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
-    @customer = CUstomer.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name,
+      :last_name_kana, :first_name_kana, :email, :postal_code,
+      :addres, :telephone_number, :is_active)
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,15 @@ Admin.create!(
   password: "arutema"
   )
 
+Customer.create!(
+  last_name: "田中",
+  first_name: "ひろし",
+  last_name_kana: "タナカ",
+  first_name_kana: "ヒロシ",
+  email: "tanaka.i@gmail.com",
+  password: "tanaka11",
+  postal_code: "8300038",
+  address: "田中県田中市",
+  telephone_number: "05032261842",
+  is_active: "true"
+  )

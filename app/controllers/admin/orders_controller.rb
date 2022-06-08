2bd945b0b6d69b3efd6f23ce7c5b2c710566fdc5
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-08 05:47:35 +0000

    [Add]注文履歴詳細着手

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,10 @@
+class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+
+  end
+end
---
+++ test/models/admin/order_detail_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Admin::OrderDetailTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

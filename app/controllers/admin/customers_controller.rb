39bc68be544ba7d920863b15ab29f4edc76bb841
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-13 06:06:03 +0000

    [Add]adminレイアウト調整完了

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -21,7 +21,7 @@ class Admin::CustomersController < ApplicationController
     def customer_params
       params.require(:customer).permit(:last_name, :first_name,
       :last_name_kana, :first_name_kana, :email, :postal_code,
-      :addres, :telephone_number, :is_active)
+      :address, :telephone_number, :is_active)
     end
 
 end

4bd70909410192fd9e5b63e3723275100000e7b5
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-20 05:20:31 +0000

    [Add]配送先編集画面実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,11 +6,25 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path(current_customer)
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path(current_customer)
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path(current_customer)
   end
 
   private

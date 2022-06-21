ce04cb9f9b25decbf372caec734748df467738c7
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-21 05:23:41 +0000

    [Add]商品一覧画面実装

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,5 +1,15 @@
 class Public::ItemsController < ApplicationController
+
   def index
+    @genres = Genre.all
+    if params[:genre_id]
+      genre = Genre.find(params[:genre_id])
+      @text = genre.name
+      @items = genre.items.page(params[:page])
+    else
+      @text = '商品'
+      @items = Item.page(params[:page])
+    end
   end
 
   def show

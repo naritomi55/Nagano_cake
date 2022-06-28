4516b0696f7aa740fbfd0b5efcb04951b3ccdd56
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-28 06:03:13 +0000

    [Add]注文情報入力画面エラー修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,20 +3,49 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
 
+  def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    if @order.save
+      cart_items.each do |cart|
+        order_item = Orderdetail.new
+        order_item.item_id = cart.item_id
+        order_item.order_id = @order.id
+        order_item.order_amount = cart.amount
+        order_item.order_price = cart.item.price
+        order_item.save
+      end
+      redirect_to orders_confirm_path
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
+  end
+
   def confirm
     @order = Order.new(order_params)
-    if
-      def select_address_0
-        @order.postal_code = current_customer.postal_code
-        @order.address = current_customer.address
-        @order.name = current_customer.full_name
-      end
-    elsif select_address_1
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.full_name
+    elsif params[:order][:select_address] == "1"
       @address = Address.find(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
+    elsif @params[:order][:select_address] == "2"
+      @order.postal_code = current_customer.postal_code.new(postal_code_params)
+      @order.address = current_customer.address.new(address_params)
+      @order.name = current_customer.name.new(name_params)
+      if address_new.save
+      else
+        render :new
+      end
     end
+    @cart_items = current_customer.cart_items.all
+    @order.shipping_cost = "800"
+    @order.total_payment = ()
   end
 
   def complete
@@ -32,6 +61,10 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name,)
+  end
+
+  def address_params
+    params.require(:order).permit(:select_address, :address_id)
   end
 end

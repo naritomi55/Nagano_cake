26e0b89bc536901e0edc5f39b1623255e8d9d21d
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-06-30 03:10:34 +0000

    [Add]注文履歴一覧実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(@order.customer_id)
     @status = Order.all
     @status.first
   end

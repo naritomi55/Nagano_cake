bc224843d230fea2b00ec94700b3d1eadd7302b7
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-07-11 05:01:55 +0000

    [Add]提出完了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,13 +4,24 @@ class Admin::OrderDetailsController < ApplicationController
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
     @order_detail.update(order_detail_params)
+
     if
       @order_detail.making_status == "c"
       @order.update(status: "c")
-    elsif
-      @order_detail.making_status == "d"
+    end
+
+    order_details = @order.order_details
+
+    is_all_d = true
+    order_details.each do |order_detail|
+      if order_detail.making_status != "d"
+        is_all_d = false
+      end
+    end
+    if is_all_d
       @order.update(status: "d")
     end
+
     redirect_to admin_order_path(@order_detail.order.id)
   end
 
